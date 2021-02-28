e90ef78b06b98388a14b09eddc8b6464f86ec0ba
worean <worean@naver.com>
2021-02-28 22:24:39 +0900

    #소스 수정 및 기본 설정

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # InstaClone Back-end
 
+Instaclone Backend.
+
+## User : 
+ - [ ] Create Account
+ - [ ] See Profile
+ - [ ] Login
+ - [ ] Edit Profile
+ - [ ] Follow User
+ - [ ] UnFollow User
+ - [ ] Change Avatar (Image Upload)
--- movies/movies.queries.js
+++
@@ -1,8 +0,0 @@
-import client from '../client'
-
-export default {
-    Query: {
-        movies: () => client.movie.findMany(), // movie테이블의 정보들을 모두 가져온다.
-        movie: (_, { id }) => client.movie.findUnique({ where: { id } }),
-      },
-}
