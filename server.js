4437e009170ef987f46c6d37e17b83a69d9e0e5a
worean <worean@naver.com>
2021-03-08 01:56:00 +0900

    # avatar 설정 기능추가

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
-import {typeDefs, resolvers} from './schema';
-import { ApolloServer } from 'apollo-server';
+require("dotenv").config();
+import express from "express";
+import logger from "morgan";
+import { typeDefs, resolvers } from './schema';
+import { ApolloServer } from 'apollo-server-express';
 import { getUser } from './users/users.utils';
 
+const PORT = process.env.PORT;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: async ({req}) => {
+  context: async ({ req }) => {
     return {
       logginedUser: await getUser(req.headers.token)
     };
   },
 });
 
-// The `listen` method launches a web server.
-export
-const PORT = process.env.PORT;
-
-server
-  .listen(PORT) 
-  .then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+const app = express();
+server.applyMiddleware({ app });
+app.use(logger("tiny"));
+// 서버에 uploads 폴더를 올린다.
+app.use(express.static("uploads"));
+app.listen({ port: PORT }, () => {
+    console.log(`🚀  Server is running on http://localhost:${PORT}/graphql`);
   });
 
 
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -8,16 +8,22 @@ const fnEditProfile = async (_,
     { firstName, lastName, email, password: newPassword, userName, bio, avatar},
     { logginedUser }
 ) => {
-    const {filename, createReadStream} = await avatar;
+    let avatar_url = null;
+    if(avatar) {
+        const {filename, createReadStream} = await avatar;
+        const newFilename = `${logginedUser.id}-${Date.now()}-${filename}`;
+        // ReadStream을 가져온다.
+        const readStream = createReadStream();
     
-    // ReadStream을 가져온다.
-    const readStream = createReadStream();
+        // 쓰기 스트림을 특정 폴더와 파일로 옮긴다. -> 파일이름은 id + 시간 + 파일이름 으로 설정된다.(중복방지)
+        const writeStream = createWriteStream(process.cwd() + "/uploads/" + newFilename);
+    
+        // 읽은 데이터를 writeStream으로 연결한다.(pipe)
+        readStream.pipe(writeStream);
 
-    // 쓰기 스트림을 특정 폴더와 파일로 옮긴다.
-    const writeStream = createWriteStream(process.cwd() + "/uploads/" + filename);
-
-    // 읽은 데이터를 writeStream으로 연결한다.(pipe)
-    readStream.pipe(writeStream);
+        // url을 저장한다.
+        avatar_url = `http://localhost:4000/static/${newFilename}`;
+    }
 
     let hashedPW = null;
     if (newPassword) {
@@ -33,7 +39,8 @@ const fnEditProfile = async (_,
             userName,
             email,
             bio,
-            ...(hashedPW && { password: hashedPW })
+            ...(hashedPW && { password: hashedPW }),
+            ...(avatar_url&& {avatar:avatar_rul})
         },
     });
     if (updateUser.id) {
