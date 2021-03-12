b51a479d2522e4b31a948d623f19b5383d499242
worean <worean@naver.com>
2021-03-12 13:45:00 +0900

    # User Type 변경

--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -13,5 +13,8 @@ export default gql`
         updatedAt:String!
         following:[User]
         followers:[User]
+        totalFollowers:Int
+        totalFollowing:Int
+        isFollowing:Boolean
     }
 `;
